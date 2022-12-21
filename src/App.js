742750d4428bc8b974a96b27fb8de027d16ea8ec
frederic <flausson@normandiewebschool.fr>
2022-12-21 14:34:24 +0100

    [ tictactoe ] jouable sur le meme pc

--- src/App.js
+++ src/App.js
@@ -1,97 +1,106 @@
+import { useEffect } from 'react';
 import './App.css';
 
 function App() {
 
-  
-const PLAYER_X_CLASS = 'x'
-const PLAYER_O_CLASS = 'circle'
-const WINNING_COMBINATIONS = [
-	[0, 1, 2],
-	[3, 4, 5],
-	[6, 7, 8],
-	[0, 3, 6],
-	[1, 4, 7],
-	[2, 5, 8],
-	[0, 4, 8],
-	[2, 4, 6]
-]
+  useEffect(() => {
+    const PLAYER_X_CLASS = 'x'
+    const PLAYER_O_CLASS = 'circle'
+    const WINNING_COMBINATIONS = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ]
 
-const cellElements = document.querySelectorAll('[data-cell]')
-const boardElement = document.getElementById('board')
-const winningMessageElement = document.getElementById('winningMessage')
-const restartButton = document.getElementById('restartButton')
-const winningMessageTextElement = document.getElementById('winningMessageText')
-let isPlayer_O_Turn = false
+    const cellElements = document.querySelectorAll('[data-cell]')
+    const boardElement = document.getElementById('board')
+    const winningMessageElement = document.getElementById('winningMessage')
+    const restartButton = document.getElementById('restartButton')
+    const winningMessageTextElement = document.getElementById('winningMessageText')
+    let isPlayer_O_Turn = false
 
-startGame()
 
-restartButton.addEventListener('click', startGame)
+    // console.log(cellElements)
+    // cellElements.forEach(cell => {
+    //       cell.classList.add(PLAYER_X_CLASS)
+    //       cell.classList.add(PLAYER_O_CLASS)
+    //       console.log("zeazeaze")
+    //     })
+    // startGame()
 
-function startGame() {
-	isPlayer_O_Turn = false
-	cellElements.forEach(cell => {
-		cell.classList.remove(PLAYER_X_CLASS)
-		cell.classList.remove(PLAYER_O_CLASS)
-		cell.removeEventListener('click', handleCellClick)
-		cell.addEventListener('click', handleCellClick, { once: true })
-	})
-	setBoardHoverClass()
-	winningMessageElement.classList.remove('show')
-}
+    restartButton.addEventListener('click', startGame)
 
-function handleCellClick(e) {
-	const cell = e.target
-	const currentClass = isPlayer_O_Turn ? PLAYER_O_CLASS : PLAYER_X_CLASS
-	placeMark(cell, currentClass)
-	if (checkWin(currentClass)) {
-		endGame(false)
-	} else if (isDraw()) {
-		endGame(true)
-	} else {
-		swapTurns()
-		setBoardHoverClass()
-	}
-}
+    function startGame() {
+    	isPlayer_O_Turn = false
+    	cellElements.forEach(cell => {
+    		cell.classList.remove(PLAYER_X_CLASS)
+    		cell.classList.remove(PLAYER_O_CLASS)
+    		cell.removeEventListener('click', handleCellClick)
+    		cell.addEventListener('click', handleCellClick, { once: true })
+    	})
+    	setBoardHoverClass()
+    	winningMessageElement.classList.remove('show')
+    }
 
-function isDraw() {
-	return [...cellElements].every(cell => {
-		return cell.classList.contains(PLAYER_X_CLASS) || cell.classList.contains(PLAYER_O_CLASS)
-	})
-}
+    function handleCellClick(e) {
+    	const cell = e.target
+    	const currentClass = isPlayer_O_Turn ? PLAYER_O_CLASS : PLAYER_X_CLASS
+    	placeMark(cell, currentClass)
+    	if (checkWin(currentClass)) {
+    		endGame(false)
+    	} else if (isDraw()) {
+    		endGame(true)
+    	} else {
+    		swapTurns()
+    		setBoardHoverClass()
+    	}
+    }
 
-function placeMark(cell, currentClass) {
-	cell.classList.add(currentClass)
-}
+    function isDraw() {
+    	return [...cellElements].every(cell => {
+    		return cell.classList.contains(PLAYER_X_CLASS) || cell.classList.contains(PLAYER_O_CLASS)
+    	})
+    }
 
-function swapTurns() {
-	isPlayer_O_Turn = !isPlayer_O_Turn
-}
+    function placeMark(cell, currentClass) {
+    	cell.classList.add(currentClass)
+    }
 
-function setBoardHoverClass() {
-	boardElement.classList.remove(PLAYER_X_CLASS)
-	boardElement.classList.remove(PLAYER_O_CLASS)
-	if (isPlayer_O_Turn) {
-		boardElement.classList.add(PLAYER_O_CLASS)
-	} else {
-		boardElement.classList.add(PLAYER_X_CLASS)
-	}
-}
+    function swapTurns() {
+    	isPlayer_O_Turn = !isPlayer_O_Turn
+    }
 
-function checkWin(currentClass) {
-	return WINNING_COMBINATIONS.some(combination => {
-		return combination.every(index => {
-			return cellElements[index].classList.contains(currentClass)
-		})
-	})
-}
-function endGame(draw) {
-	if (draw) {
-		winningMessageTextElement.innerText = "It's a draw!"
-	} else {
-		winningMessageTextElement.innerText = `Player with ${isPlayer_O_Turn ? "O's" : "X's"} wins!`
-	}
-	winningMessageElement.classList.add('show')
-}
+    function setBoardHoverClass() {
+    	boardElement.classList.remove(PLAYER_X_CLASS)
+    	boardElement.classList.remove(PLAYER_O_CLASS)
+    	if (isPlayer_O_Turn) {
+    		boardElement.classList.add(PLAYER_O_CLASS)
+    	} else {
+    		boardElement.classList.add(PLAYER_X_CLASS)
+    	}
+    }
+
+    function checkWin(currentClass) {
+    	return WINNING_COMBINATIONS.some(combination => {
+    		return combination.every(index => {
+    			return cellElements[index].classList.contains(currentClass)
+    		})
+    	})
+    }
+    function endGame(draw) {
+    	if (draw) {
+    		winningMessageTextElement.innerText = "It's a draw!"
+    	} else {
+    		winningMessageTextElement.innerText = `Player with ${isPlayer_O_Turn ? "O's" : "X's"} wins!`
+    	}
+    	winningMessageElement.classList.add('show')
+    }
+  })
 
   return (
     <div className='container'>
@@ -115,10 +124,5 @@ function endGame(draw) {
     </div>
   );
 }
-
-// tic tac toe
-
-
-
 
 export default App;
